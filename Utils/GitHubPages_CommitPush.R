90599c5cbee8783785f27da262a8b8c230ce2c6a
sdesabbata <stefano.desabbata@gmail.com>
2018-10-12 19:25:33 +0100

    Util to commit and push GitHub Pages

---
+++ Utils/GitHubPages_CommitPush.R
@@ -0,0 +1,7 @@
+commit_comment <- "Update GitHub pages"
+
+system("git checkout gh-pages")
+system("git merge master")
+system("paste(git commit -am"), commit_comment))
+system("git push")
+system("git checkout master")
