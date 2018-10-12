ac9704ef06379209b103bde1c29ef146549cd5fb
sdesabbata <stefano.desabbata@gmail.com>
2018-10-12 19:26:58 +0100

    Fixed incorrect quote

--- Utils/GitHubPages_CommitPush.R
+++ Utils/GitHubPages_CommitPush.R
@@ -2,6 +2,6 @@ commit_comment <- "Update GitHub pages"
 
 system("git checkout gh-pages")
 system("git merge master")
-system("paste(git commit -am"), commit_comment))
+system(paste("git commit -am", commit_comment))
 system("git push")
 system("git checkout master")
