dd4ff6248ad66465677881bc938fd72cfd9fa9e0
Stefano De Sabbata <stefano.desabbata@gmail.com>
2018-10-23 23:53:33 +0100

    Updated Make.R file

--- Make.R
+++ Make.R
@@ -5,10 +5,20 @@
 ##########
 
 # Step 0: install tidyverse
-install.packages("tidyverse")
+# Un comment the install.packages command below
+# if the Tidyverse is not installed
+#install.packages("tidyverse")
+
+library(rmarkdown)
 
 # Step 1: execute the scripts that gather data
 # from Ofcom
 source("Data/Gather_Ofcom_data_2012.R")
 # and from the Department for Transport
 source("Data/Gather_DfT_data_2015.R")
+
+# Compile the lecture file
+rmarkdown::render("Materials/Lecture/ReproducibleResearchWithR.Rmd")
+
+# Compile the analysis document for the practical session
+rmarkdown::render("Materials/Lecture/ReproducibleResearchWithR.Rmd")
